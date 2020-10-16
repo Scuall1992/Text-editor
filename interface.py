01bb8e6f4455e8ba6a58aa3e5822c31de86ea12a
Scuall <scuall1992@gmail.com>
2020-10-16 07:23:00 +0300

    transform to class

--- interface.py
+++ interface.py
@@ -2,43 +2,58 @@ from tkinter import *
 from tkinter import scrolledtext
 import library
 
-window = Tk()
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+                
+        self.master.title("Text editor")
+        self.master.geometry("800x600")
+
+        self.textarea = scrolledtext.ScrolledText(self.master,width=99,height=20)
+        self.buffer = scrolledtext.ScrolledText(self.master,width=99,height=10, state=DISABLED)
+        
+        self.buffer.grid(row=3, column=0, columnspan=4)
+        self.input_word = Entry(self.master,width=30)
+        self.label_input_word = Label(self.master, text="Input word: ")
+
+        self.textarea.grid(row=0, column=0, columnspan=4)
+        self.label_input_word.grid(row=1, column=0)
+        self.input_word.grid(row=1, column=1)
+
+        # Create a Tkinter variable
+        self.tkvar = StringVar(self.master)
+
+        # Dictionary with options
+        self.choices = { 'Pizza','Lasagne','Fries','Fish','Potatoe'}
+        self.tkvar.set('Pizza') # set the default option
+
+        self.popupMenu = OptionMenu(self.master, self.tkvar, *self.choices)
+        Label(self.master, text="Choose a command").grid(row = 2, column=0)
+        self.popupMenu.grid(row = 2, column=1)
+
+        # on change dropdown value
+        def change_dropdown(*args):
+            print( self.tkvar.get() )
+
+        # link function to change dropdown
+        self.tkvar.trace('w', change_dropdown)
+
+        Button(self.master, text="Run command", command=self.insert_text).grid(row=2, column=2)
+        Button(self.master, text="Save buffer", command=self.save_buffer).grid(row=4, column=0)
+        
+    def insert_text(self):
+        self.buffer.insert(END, "Hello")
 
 
-window.title("Text editor")
-window.geometry("800x600")
+    def save_buffer(self):
+        print(self.buffer.get("1.0",END))
+
+    def run(self):
+        self.master.mainloop()
 
 
 
-textarea = scrolledtext.ScrolledText(window,width=99,height=20)
-buffer = scrolledtext.ScrolledText(window,width=99,height=10, state=DISABLED).grid(row=3, column=0, columnspan=4)
-input_word = Entry(window,width=30)
-label_input_word = Label(window, text="Input word: ")
-
-textarea.grid(row=0, column=0, columnspan=4)
-label_input_word.grid(row=1, column=0)
-input_word.grid(row=1, column=1)
-
-# Create a Tkinter variable
-tkvar = StringVar(window)
-
-# Dictionary with options
-choices = { 'Pizza','Lasagne','Fries','Fish','Potatoe'}
-tkvar.set('Pizza') # set the default option
-
-popupMenu = OptionMenu(window, tkvar, *choices)
-Label(window, text="Choose a command").grid(row = 2, column=0)
-popupMenu.grid(row = 2, column=1)
-
-# on change dropdown value
-def change_dropdown(*args):
-    print( tkvar.get() )
-
-# link function to change dropdown
-tkvar.trace('w', change_dropdown)
-
-Button(window, text="Run command").grid(row=2, column=2)
-Button(window, text="Save buffer").grid(row=4, column=0)
 
 
 '''
@@ -47,4 +62,4 @@ Button(window, text="Save buffer").grid(row=4, column=0)
 
 '''
 
-window.mainloop()
+MyFirstGUI(Tk()).run()
